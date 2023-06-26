d5f7f0051efa974e7f9327a68fdb5b2679dd62aa
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-26 16:53:38 -0700

    Update Greet-developers.js

--- Greet-developers.js
+++ Greet-developers.js
@@ -35,3 +35,8 @@ function greetDevelopers(list) {
 }
 
 // or
+
+function greetDevelopers(list) {
+  for (let i = 0; i < list.length; ++i)
+    list[i]['greeting']='Hi '+list[i].firstName+', what do you like the most about '+list[i].language+'?';
+}
